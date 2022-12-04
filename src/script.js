83c987dc981a49135544d14a0ade8809731f3b0e
harvey <106120222+rachelynne-harvey@users.noreply.github.com>
2022-12-04 13:15:16 -0600

    updated script to include unit conversion
    added in global variables for celsius temperature
    
    added in function to convert from fahrenheit to celsius upon fahrenheit link click
    
    added in function to convert back to celsius upon celsius link click

--- src/script.js
+++ src/script.js
@@ -8,26 +8,28 @@ function changeCity(event) {
   axios.get(url).then(alertChange);
 }
 
-//alerts that you are changing information after submit
+//alerts that you are changing information after submit and then changes it
 function alertChange(response) {
   let cityChange = document.querySelector("h4");
-  let currentTemp = document.querySelector("span#current-temp");
+  let currentTemp = document.querySelector('span#current-temp');
   let highTemp = document.querySelector("span#high-degrees");
   let lowTemp = document.querySelector("span#low-degrees");
-  let currentConditions = document.querySelector("span#current-conditions");
-  let humidity = document.querySelector("span#humidity");
-  let windSpeed = document.querySelector("span#wind-speed");
-  let weatherIcon = document.querySelector("#icon");
+  let currentConditions = document.querySelector('span#current-conditions');
+  let humidity = document.querySelector('span#humidity');
+  let windSpeed = document.querySelector('span#wind-speed');
+  let weatherIcon = document.querySelector('#icon');
   alert(`let's find out the weather in ${response.data.city}, shall we?`);
-  console.log(response);
+  currentCelsius = Math.round(response.data.daily[0].temperature.day);
+  highCelsius = Math.round(response.data.daily[0].temperature.maximum);
+  lowCelsius = Math.round(response.data.daily[0].temperature.minimum);
   cityChange.innerHTML = response.data.city;
-  currentTemp.innerHTML = Math.round(response.data.daily[0].temperature.day);
-  highTemp.innerHTML = Math.round(response.data.daily[0].temperature.maximum);
-  lowTemp.innerHTML = Math.round(response.data.daily[0].temperature.minimum);
+  currentTemp.innerHTML = currentCelsius;
+  highTemp.innerHTML = highCelsius;
+  lowTemp.innerHTML = lowCelsius;
   currentConditions.innerHTML = response.data.daily[0].condition.description;
   humidity.innerHTML = response.data.daily[0].temperature.humidity;
   windSpeed.innerHTML = Math.round(response.data.daily[0].wind.speed);
-  weatherIcon.setAttribute("src", response.data.daily[0].condition.icon_url);
+  weatherIcon.setAttribute('src', response.data.daily[0].condition.icon_url);
 }
 
 // find current coordinates for user
@@ -45,6 +47,45 @@ function findLocation(position) {
   axios.get(url).then(alertChange);
 }
 
+// converts temperature from celsius to fahrenheit
+function convertFahrenheitTemperature (event) {
+  event.preventDefault();
+  let currentFahrenheit = document.querySelector('#current-temp');
+  let highFahrenheit = document.querySelector('#high-degrees');
+  let lowFahrenheit = document.querySelector('#low-degrees');
+  let unitElement = document.querySelector('#unit');
+  let fahrenheitCurrent = (currentCelsius * 9) / 5 + 32;
+  let fahrenheitHigh = (highCelsius * 9) / 5 + 32;
+  let fahrenheitLow = (lowCelsius * 9) / 5 + 32;
+  alert('changing to fahrenheit right now!');
+  unitElement.innerHTML = ('F')
+  currentFahrenheit.innerHTML = Math.round(fahrenheitCurrent);
+  highFahrenheit.innerHTML = Math.round(fahrenheitHigh);
+  lowFahrenheit.innerHTML = Math.round(fahrenheitLow);
+}
+
+// converts temperature from fahrenheit to celsius
+function convertCelsiusTemperature (event) {
+  event.preventDefault();
+  let currentCelsiusTemp = document.querySelector('#current-temp');
+  let highCelsiusTemp = document.querySelector('#high-degrees');
+  let lowCelsiusTemp = document.querySelector('#low-degrees');
+  let unitElement = document.querySelector('#unit');
+  let celsiusCurrent = currentCelsius;
+  let celsiusHigh = highCelsius;
+  let celsiusLow = lowCelsius;
+  alert ('changing to celsius right now!');
+  unitElement.innerHTML = ('C');
+  currentCelsiusTemp.innerHTML = Math.round(celsiusCurrent);
+  highCelsiusTemp.innerHTML = Math.round(celsiusHigh);
+  lowCelsiusTemp.innerHTML = Math.round(celsiusLow);
+}
+
+// establishes celsius temperature to use in functions
+let currentCelsius = null;
+let highCelsius = null;
+let lowCelsius = null;
+
 // starts function calls when you click on 'current location' button
 let currentLocationLink = document.querySelector("#current-location-button");
 currentLocationLink.addEventListener("click", currentLocation);
@@ -52,3 +93,11 @@ currentLocationLink.addEventListener("click", currentLocation);
 // starts function calls when you click on 'search' button
 let menuSearch = document.querySelector("#menu-submit");
 menuSearch.addEventListener("click", changeCity);
+
+// starts function calls to convert to fahrenheit
+let convertCelsius = document.querySelector('#fahrenheit-type');
+convertCelsius.addEventListener('click', convertFahrenheitTemperature);
+
+// starts function calls to convert to celsius
+let convertFahrenheit = document.querySelector('#celsius-type');
+convertFahrenheit.addEventListener('click', convertCelsiusTemperature)
