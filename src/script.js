413201f5c856c6ceeb4ea1d310fa2368392f8bdc
harvey <106120222+rachelynne-harvey@users.noreply.github.com>
2022-12-03 18:05:17 -0600

    updated api documentation
    changed api to shecodes weather api instead of open weather api that was being used before

--- src/script.js
+++ src/script.js
@@ -3,29 +3,29 @@ function changeCity(event) {
   event.preventDefault();
   let cityInput = document.querySelector("#city-input");
   let city = cityInput.value;
-  let apiKey = "7fc03e8d4259be6572769ab994d325bb";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiKey = "a04dt03595dcf73o40ef02782a9109ba";
+  let url = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(url).then(alertChange);
 }
 
 //alerts that you are changing information after submit
 function alertChange(response) {
   let cityChange = document.querySelector("h4");
-  let currentTemp = document.querySelector('span#current-temp');
+  let currentTemp = document.querySelector("span#current-temp");
   let highTemp = document.querySelector("span#high-degrees");
   let lowTemp = document.querySelector("span#low-degrees");
-  let currentConditions = document.querySelector('span#current-conditions');
-  let humidity = document.querySelector('span#humidity');
-  let windSpeed = document.querySelector('span#wind-speed');
-  alert(`let's find out the weather in ${response.data.name}, shall we?`);
+  let currentConditions = document.querySelector("span#current-conditions");
+  let humidity = document.querySelector("span#humidity");
+  let windSpeed = document.querySelector("span#wind-speed");
+  alert(`let's find out the weather in ${response.data.city}, shall we?`);
   console.log(response);
-  cityChange.innerHTML = response.data.name;
-  currentTemp.innerHTML = Math.round(response.data.main.temp);
-  highTemp.innerHTML = Math.round(response.data.main.temp_max);
-  lowTemp.innerHTML = Math.round(response.data.main.temp_min);
-  currentConditions.innerHTML = response.data.weather[0].description;
-  humidity.innerHTML = response.data.main.humidity;
-  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  cityChange.innerHTML = response.data.city;
+  currentTemp.innerHTML = Math.round(response.data.daily[0].temperature.day);
+  highTemp.innerHTML = Math.round(response.data.daily[0].temperature.maximum);
+  lowTemp.innerHTML = Math.round(response.data.daily[0].temperature.minimum);
+  currentConditions.innerHTML = response.data.daily[0].condition.description;
+  humidity.innerHTML = response.data.daily[0].temperature.humidity;
+  windSpeed.innerHTML = Math.round(response.data.daily[0].wind.speed);
 }
 
 // find current coordinates for user
@@ -36,10 +36,10 @@ function currentLocation(event) {
 
 // shows weather for current user location
 function findLocation(position) {
-  let apiKey = "7fc03e8d4259be6572769ab994d325bb";
+  let apiKey = "a04dt03595dcf73o40ef02782a9109ba";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
+  let url = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=imperial`;
   axios.get(url).then(alertChange);
 }
 
